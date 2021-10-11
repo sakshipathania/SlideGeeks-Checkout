bbf4a5245e54ba8b4762834a1f1924de406fc9f6
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-10-11 10:34:36 +0530

    change locator . name, new_email.

--- src/test/java/SetupClass/StepDefinition/Paypal_Checkout.java
+++ src/test/java/SetupClass/StepDefinition/Paypal_Checkout.java
@@ -61,8 +61,9 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(3000);
 		driver.get("https://www.slidegeeks.com/register");
 		Thread.sleep(3000);
-		WebElement name = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#jform_name1")));
+		WebElement name = wait.until(ExpectedConditions.elementToBeClickable(By.name("jform[name1]")));
 		 Thread.sleep(3000);
+		name.clear();
 		 name.sendKeys("Automated Program");
 		 Thread.sleep(3000);
 	   
@@ -88,7 +89,7 @@ public class Paypal_Checkout extends SetupClass {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);	
 		Thread.sleep(2000);
 		
-		WebElement new_email = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#jform_email1")));
+		WebElement new_email = wait.until(ExpectedConditions.elementToBeClickable(By.name("jform[email1]")));
 		 Thread.sleep(3000);
 		 new_email.sendKeys(full_email);
 		 Thread.sleep(3000);
