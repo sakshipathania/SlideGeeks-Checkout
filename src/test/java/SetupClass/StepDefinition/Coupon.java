d340306ccbaebf8e285ad0c8be9a3fc295259033
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-11-30 18:05:54 +0530

    Update Coupon.java

--- src/test/java/SetupClass/StepDefinition/Coupon.java
+++ src/test/java/SetupClass/StepDefinition/Coupon.java
@@ -133,8 +133,8 @@ public class Coupon extends SetupClass {
     @Then("^user is redirected to pricing page and choose the plan to pay cc$")
 	public void user_is_redirected_to_pricing_page_and_choose_the_plan_to_pay_cc() throws Throwable {
 		// choose a plan
-		//driver.get("https://www.slidegeeks.com/subscriptions");
-		//Thread.sleep(3000);
+		driver.get("https://www.slidegeeks.com/subscriptions");
+		Thread.sleep(3000);
 		 WebElement Subscribe_btn  =  driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/span/form/span/button"));
 		js.executeScript("arguments[0].scrollIntoView();",Subscribe_btn);
 			Thread.sleep(2000);
