2485496f0f97ae4cae6b057c41c42ad6e2901bc4
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-12-02 17:14:31 +0530

    Update Email_Sign_up_Correct_Data_2.java

--- src/test/java/SetupClass/StepDefinition/Email_Sign_up_Correct_Data_2.java
+++ src/test/java/SetupClass/StepDefinition/Email_Sign_up_Correct_Data_2.java
@@ -2,7 +2,7 @@ package SetupClass.StepDefinition;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
