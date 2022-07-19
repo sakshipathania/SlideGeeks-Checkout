50ec97d2970d546faf9df4a454093f18af14a68d
nishaslidetech <nisha.dhiman@slidetech.in>
2022-07-19 13:33:52 +0530

    update captcha

--- src/test/java/SetupClass/TestRunnner.java
+++ src/test/java/SetupClass/TestRunnner.java
@@ -8,8 +8,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "." }, tags = ("@Paypal_Checkout"), plugin = { "pretty", "html:target/site/cucumber-pretty",
-		"json:target/cucumber/cucumber.json", "usage:target/usage.jsonx", "junit:target/cucumber.xml" })
+@CucumberOptions(features = { "." }, // tags = ("@Paypal_Checkout"),
+		plugin = { "pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json",
+				"usage:target/usage.jsonx", "junit:target/cucumber.xml" })
 
 public class TestRunnner {
 
